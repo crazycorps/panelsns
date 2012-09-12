40fce1901db6d1d0cc83603cd6bda9fe0bc01a0d
corps <crazycorps@163.com>
2012-09-12 17:55:56 +0800

    sn update

--- panelsns-service/src/main/java/com/survey/panelsns/service/vo/QuesVO.java
+++ panelsns-service/src/main/java/com/survey/panelsns/service/vo/QuesVO.java
@@ -1,19 +1,36 @@
 package com.survey.panelsns.service.vo;
 
-import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.binary.Hex;
 
 import com.survey.panelsns.model.entity.Ques;
 import com.survey.service.vo.AbstractObjectVO;
+import com.survey.tools.encrypt.AESCoder;
+import com.survey.tools.util.BitUtils;
 
 @SuppressWarnings("serial")
 public class QuesVO extends AbstractObjectVO<Ques> {
-
-	private static final String PREFIX="q_";
 	
+	private static final byte[] SECRET_KEY=AESCoder.initSecretKey();
+
 	public String getSn(){
 		if(this.entity==null||this.entity.getId()==null){
 			return null;
 		}
-		return DigestUtils.md5Hex(PREFIX+this.entity.getId());
+		try {
+			return Hex.encodeHexString(AESCoder.encrypt(BitUtils.long2bytes(this.entity.getId()), SECRET_KEY));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public long parseSn(String sn){
+		try {
+			byte[] data = AESCoder.decrypt(sn.getBytes(), SECRET_KEY);
+			return BitUtils.bytes2long(data, 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1L;
 	}
 }
