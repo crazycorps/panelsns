bb7ea3c5c5e44349df838f22b54bccad500dcb25
corps <crazycorps@163.com>
2012-09-12 18:28:51 +0800

    sn method update

--- panelsns-service/src/main/java/com/survey/panelsns/service/vo/QuesOptionVO.java
+++ panelsns-service/src/main/java/com/survey/panelsns/service/vo/QuesOptionVO.java
@@ -6,5 +6,14 @@ import com.survey.service.vo.AbstractObjectVO;
 @SuppressWarnings("serial")
 public class QuesOptionVO extends AbstractObjectVO<QuesOption> {
 
+	private static final byte[] SECRET_KEY=new byte[]{-83, -122, -45, 86, -126, 78, 8, 1, -44, 50, 127, -26, -92, -47, 73, -27};
 
+	public String getSn(){
+		if(this.entity==null||this.entity.getId()==null){
+			return null;
+		}
+		return toSn(this.entity.getId(),SECRET_KEY);
+	}
+	
+	
 }
--- panelsns-service/src/main/java/com/survey/panelsns/service/vo/QuesVO.java
+++ panelsns-service/src/main/java/com/survey/panelsns/service/vo/QuesVO.java
@@ -1,36 +1,23 @@
 package com.survey.panelsns.service.vo;
 
-import org.apache.commons.codec.binary.Hex;
-
 import com.survey.panelsns.model.entity.Ques;
 import com.survey.service.vo.AbstractObjectVO;
-import com.survey.tools.encrypt.AESCoder;
-import com.survey.tools.util.BitUtils;
 
 @SuppressWarnings("serial")
 public class QuesVO extends AbstractObjectVO<Ques> {
 	
-	private static final byte[] SECRET_KEY=AESCoder.initSecretKey();
+	private static final byte[] SECRET_KEY=new byte[]{94, 20, 91, 23, -125, -71, 109, -121, -62, -19, 108, 4, 97, 43, -35, -29};
 
 	public String getSn(){
 		if(this.entity==null||this.entity.getId()==null){
 			return null;
 		}
 		try {
-			return Hex.encodeHexString(AESCoder.encrypt(BitUtils.long2bytes(this.entity.getId()), SECRET_KEY));
+			return toSn(this.entity.getId(), SECRET_KEY);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
-	public long parseSn(String sn){
-		try {
-			byte[] data = AESCoder.decrypt(sn.getBytes(), SECRET_KEY);
-			return BitUtils.bytes2long(data, 0);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return -1L;
-	}
 }
